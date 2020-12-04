5aa35acf686b2b40c09527419cf3c09467bd6ef4
Brad Herring <hbradherring@aol.com>
2020-12-04 10:40:12 -0500

    Prediction script added

--- main.py
+++ main.py
@@ -24,9 +24,44 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=1)
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1)
+predictions = model.predict(test_images)
 
-print('Test accuracy:', test_acc)
+COLOR = 'white'
+plt.rcParams['text.color'] = COLOR
+plt.rcParams['axes.labelcolor'] = COLOR
 
+def predict(model, image, correct_label):
+  class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+  prediction = model.predict(np.array([image]))
+  predicted_class = class_names[np.argmax(prediction)]
+
+  show_image(image, class_names[correct_label], predicted_class)
+
+
+def show_image(img, label, guess):
+  plt.figure()
+  plt.imshow(img, cmap=plt.cm.binary)
+  plt.title("Excpected: " + label)
+  plt.xlabel("Guess: " + guess)
+  plt.colorbar()
+  plt.grid(False)
+  plt.show()
+
+
+def get_number():
+  while True:
+    num = input("Pick a number: ")
+    if num.isdigit():
+      num = int(num)
+      if 0 <= num <= 1000:
+        return int(num)
+    else:
+      print("Try again...")
+
+num = get_number()
+image = test_images[num]
+label = test_labels[num]
+predict(model, image, label)
